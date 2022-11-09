e72a3ad28759ba779ca308aab8cc178c053d9c37
John Zacarias Jekel <JZJisawesome123@gmail.com>
2022-11-09 12:51:38 -0500

    Begin adding some initial code

--- src/gui.rs
+++ src/gui.rs
@@ -1,4 +1,7 @@
 
 fn main() {
-    println!("Hello, world!");
+    //Print version info
+    println!("{}\n\n", vsemur::vsemur_version_string());
+
+    println!("Hello, world! (gui)");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,21 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+//Thanks https://stackoverflow.com/questions/67307526/is-it-possible-to-get-the-cargo-environment-variable-information-for-one-crate-u
+
+pub fn vsemur_version_major() -> u128 {
+    return env!("CARGO_PKG_VERSION_MAJOR").parse::<u128>().unwrap();
+}
+
+pub fn vsemur_version_minor() -> u128 {
+    return env!("CARGO_PKG_VERSION_MINOR").parse::<u128>().unwrap();
+}
+
+pub fn vsemur_version_patch() -> u128 {
+    return env!("CARGO_PKG_VERSION_PATCH").parse::<u128>().unwrap();
+}
+
+pub fn vsemur_version_string() -> String {
+    return format!("libvsemur v{}.{}.{}", env!("CARGO_PKG_VERSION_MAJOR"), env!("CARGO_PKG_VERSION_MINOR"), env!("CARGO_PKG_VERSION_PATCH"));
+}
