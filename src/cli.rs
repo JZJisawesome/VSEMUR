064c1c4c60269a45cff74ca977fa0be2a6c08874
John Zacarias Jekel <JZJisawesome123@gmail.com>
2022-11-09 14:09:31 -0500

    Implement argument checking in cli/gui

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,18 @@
 
 fn main() {
-    //let args: Vec<String> = std::env::args().collect();
-
     //Print version info
     println!("{}\n\n", vsemur::about::version::string());
-    //println!("{}\n\n", vsemur::about::LICENSE);
 
-    println!("Hello, world! (cli)");
+    //Handle command line arguments
+    if std::env::args().len() != 2 {
+        println!("Error: Expected 1 argument (path to rom or --version)\n");
+        return;
+    }
+    let arg: String = std::env::args().nth(1).unwrap();
+    if arg == "--version" {
+        println!("{}", vsemur::about::LICENSE);
+        return
+    }
+
+    println!("Hello, world! (cli)");//TODO other things here
 }
--- src/gui.rs
+++ src/gui.rs
@@ -3,5 +3,16 @@ fn main() {
     //Print version info
     println!("{}\n\n", vsemur::about::version::string());
 
-    println!("Hello, world! (gui)");
+    //Handle command line arguments
+    if std::env::args().len() != 2 {
+        println!("Error: Expected 1 argument (path to rom or --version)\n");
+        return;
+    }
+    let arg: String = std::env::args().nth(1).unwrap();
+    if arg == "--version" {
+        println!("{}", vsemur::about::LICENSE);
+        return
+    }
+
+    println!("Hello, world! (cli)");//TODO other things here
 }
