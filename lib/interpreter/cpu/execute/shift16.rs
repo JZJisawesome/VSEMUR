be9db070d9257109f68a1fed82006e8ad6777710
John Zacarias Jekel <JZJisawesome123@gmail.com>
2022-11-16 08:19:20 -0500

    Remove old execute; we've more than surpassed feature parity with it

---
+++ lib/interpreter/cpu/execute/shift16.rs
@@ -0,0 +1,51 @@
+/* NAME//TODO
+ * By: John Jekel
+ *
+ * TODO description
+ *
+*/
+
+//TODO remove this once everything is implemented
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#[allow(unused_imports)]
+
+/* Imports */
+
+use crate::debug_panic;
+use crate::logging::log;
+use crate::logging::log_noln;
+use crate::logging::log_finln;
+use crate::interpreter::memory::MemoryState;
+use super::super::CPUState;
+use super::super::decode::*;//TODO only import what is needed from here
+
+/* Imports */
+
+//TODO (include "use" and "mod" here)
+
+/* Constants */
+
+//TODO
+
+/* Macros */
+
+//TODO (also pub(crate) use the_macro statements here too)
+
+/* Static Variables */
+
+//TODO
+
+/* Types */
+
+//TODO
+
+/* Associated Functions and Methods */
+
+//TODO
+
+/* Functions */
+
+pub(super) fn execute(cpu: &mut CPUState, mem: &mut MemoryState, rd: DecodedRegister, op: DecodedLSFTOp, rs: DecodedRegister) {
+    todo!();//TODO
+}
